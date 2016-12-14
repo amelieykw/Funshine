f8775fff29499ddf353352a218a3996b178d7673
amelieykw <amelieykw1991@gmail.com>
2016-12-14 02:41:46 +0100

    create a weather forecasting android app for using api and http requests

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a weather forecasting Android application following the udemy cours Android N : From Beginner to Paid Professional.
+
---
+++ app/src/main/java/com/yukaiwen/funshine/WeatherActivity.java
@@ -0,0 +1,14 @@
+package com.yukaiwen.funshine;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
